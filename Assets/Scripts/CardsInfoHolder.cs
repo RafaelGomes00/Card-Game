c2fe3424ab90338b854d8cfc4c55f800a630d24a
Rafael Gomes Pinheiro <61011140+RafaelGomes00@users.noreply.github.com>
2025-06-10 17:44:48 -0300

    Fix card matching on android

--- Assets/Scripts/CardsInfoHolder.cs
+++ Assets/Scripts/CardsInfoHolder.cs
@@ -1,21 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Create new card", fileName = "New card")]
 public class CardsInfoHolder : ScriptableObject
 {
     [SerializeField] private Sprite card;
-    public string identifier { get; private set; }
+    public string identifier;
 
     public CardData GetCardData()
     {
         return new CardData(card, identifier);
-    }
-
-    private void OnValidate()
-    {
-        identifier = card.GetHashCode().ToString();
     }
 }
